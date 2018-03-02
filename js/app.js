35cc8d4c1b8131df2c176bcd4849827e5ee8c2ae
Jonathan Kok <kui825@gmail.com>
2018-03-02 18:11:04 -0500

    war is now fully functional, apply css to make it look nice

--- js/app.js
+++ js/app.js
@@ -7,30 +7,36 @@ $(document).ready(function() {
   $("#playerScore h4").html(playerScore);
   $("#dealerScore h4").html(dealerScore);
   $(".dealHand button").click(() => {
-
     //when deal hand button is clicked, deck is being popped to player and dealer
 
     let dealersHand = dealersCard(shuffleDeck);
     let playersHand = playersCard(shuffleDeck);
     let playerSuit = suits(playersHand[0].suit);
     let dealerSuit = suits(dealersHand[0].suit);
-    console.log('playersHand =', playersHand[0]);
-    let winning = (winner(playersHand, dealersHand));
-    if(winning === "dealer win"){
+    console.log("playersHand =", playersHand[0]);
+    let winning = winner(playersHand, dealersHand);
+    if (winning === "dealer win") {
       dealerScore = dealerScore + 1;
-    } else if(winning === "player win"){
+    } else if (winning === "player win") {
       playerScore = playerScore + 1;
     }
 
-    $("#dealersCard").prepend(`<img class="dealersCard" src='img/JPEG/${dealersHand[0].rank}${dealerSuit}.jpg'>`);
 
-    $("#playersCard").prepend(`<img class="playersCard" src='img/JPEG/${playersHand[0].rank}${playerSuit}.jpg'>`);
+    $(".dealersCard").attr(
+      "src",
+      `img/JPEG/${dealersHand[0].rank}${dealerSuit}.jpg`
+    );
+
+    $(".playersCard").attr(
+      "src",
+      `img/JPEG/${playersHand[0].rank}${playerSuit}.jpg`
+    );
+
+    // $("#dealersCard img[src='']").prepend(` class="dealersCard" src='img/JPEG/${dealersHand[0].rank}${dealerSuit}.jpg'`);
 
     $("#dealerScore h4").html(dealerScore);
+    $("#playerScore h4").html(playerScore);
+
     $(".winner h5").html(winner(playersHand, dealersHand));
   });
-
-
 });
-
-
